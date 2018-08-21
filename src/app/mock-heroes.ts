af2535553b9f980b1279b362513f6eee13a93b17
Adel Moreno <adel.argenix@gmail.com>
2018-08-21 15:41:28 -0600

    got hero list up and running

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'The Cloud' },
+	{ id : 12, name: 'Mr. Green'},
+	{ id: 13, name: 'The Light'},
+	{ id: 14, name: 'The Dark'},
+	{ id: 15, name: 'Dynamo'},
+	{ id: 16, name: 'Dr. Thunder'},
+	{ id: 17, name: 'The Squeeze'},
+	{ id: 18, name: 'Transforma'},
+	{ id: 19, name: 'The Boulder'},
+	{ id: 20, name: 'Roger'}
+];
