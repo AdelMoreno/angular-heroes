2dc31cb518e628164d78a324df3b61fbb7f95ad8
Adel Moreno <adel.argenix@gmail.com>
2018-08-21 16:12:53 -0600

    added function to get promises

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService} from '../hero.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -9,12 +9,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-	heroes = HEROES;
+	heroes: HERO[];
 
 
-	constructor() { }
+	constructor(private heroService: HeroService) { }
 
 	ngOnInit() {
+		this.getHeroes();
 	}
 
 	selectedHero: Hero;
@@ -22,5 +23,10 @@ export class HeroesComponent implements OnInit {
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 	}
+
+getHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes);
+}
 
 }
